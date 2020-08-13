5ec237a76d0098649fabebfd4c8077a4ef01c66b
Honore Hounwanou <mercuryseries@gmail.com>
2020-08-13 01:26:28 -0400

    Install Maker bundle and setup PagesController

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function home(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PagesController.php',
+        ]);
+    }
+}
